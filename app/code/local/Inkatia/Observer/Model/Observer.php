219b6f3738f1d36b2cf82907ea679e316cbf54b7
Tiago Gomes <tiago.gomes@localhost.localdomain>
2013-11-11 19:30:03 +0000

    first commit

---
+++ app/code/local/Inkatia/Observer/Model/Observer.php
@@ -0,0 +1,16 @@
+<?php
+class Inkatia_Observer_Model_Observer
+{
+	public function callObserver($observer)
+	{
+		/*
+		 * Get Invoice, Get Order, Do Whatever you wanna do!
+		 */
+		$invoice = $observer->getEvent()->getInvoice();
+		$order   = $observer->getEvent()->getOrder();
+		
+		Mage::log("Observer: working perfectly!");
+		
+		
+	}
+}
